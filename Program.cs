361e3fc89f1e9ab7fdb19f88275df2c266c4ee19
Francisco Javier Chacon Aguirre <Francisco Chacon@compac.com.mx>
2020-07-07 12:46:15 -0500

    Initial Commit - Moving Project

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace ListFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string vPath = @"C:\Users\Francisco Chacon\Documents\CONTPAQi\TALENTO HUMANO\Capacitacion\JAVA\JavaCourse";
+            string vDestinationPath = @"C:\Users\Francisco Chacon\Documents\CONTPAQi\TALENTO HUMANO\destionation.txt";
+            string[] vFiles;
+
+            vFiles = Directory.GetFiles(vPath);
+
+            foreach(string s in vFiles)
+            {
+                Console.WriteLine(s);
+                File.AppendAllText(vDestinationPath,s + "\n");
+            }
+        }
+    }
+}
